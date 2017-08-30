862b58e8a400b20da161d5e5ee037cfcc2f17286
Readdle <readdle@Readdles-MacBook-Pro-2.local>
2017-08-30 18:01:06 +0300

    Initial Commit

---
+++ Chroma/Chroma.h
@@ -0,0 +1,19 @@
+//
+//  Chroma.h
+//  Chroma
+//
+//  Created by Readdle on 19.07.17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Chroma.
+FOUNDATION_EXPORT double ChromaVersionNumber;
+
+//! Project version string for Chroma.
+FOUNDATION_EXPORT const unsigned char ChromaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chroma/PublicHeader.h>
+
+
---
+++ config.h
@@ -0,0 +1,16 @@
+#define HAVE_ROUND 1
+#define HAVE_LRINTF 1
+/* #undef HAVE_AV_PACKET_UNREF */
+/* #undef HAVE_AV_FRAME_ALLOC */
+/* #undef HAVE_AV_FRAME_FREE */
+
+#define TESTS_DIR "/Users/readdle/Desktop/chromaprint-master2/tests/"
+
+/* #undef USE_SWRESAMPLE */
+/* #undef USE_AVRESAMPLE */
+
+/* #undef USE_AVFFT */
+/* #undef USE_FFTW3 */
+/* #undef USE_FFTW3F */
+#define USE_VDSP 1
+/* #undef USE_KISSFFT */
